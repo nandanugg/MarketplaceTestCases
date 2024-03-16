35f3738ea553559d217ba1c6c8c44f28331b35e9
nandanugg <nandanugg@gmail.com>
2024-03-16 12:48:59 +0700

    add verbosity

--- tests/registerTest.js
+++ tests/registerTest.js
@@ -41,7 +41,7 @@ export function RegistrationTest(user, doNegativeCase) {
         password: generateRandomPassword()
     }, { headers: { 'Content-Type': 'application/json' } })
 
-    console.log(r.json())
+    console.log(res.json())
     check(res, {
         [TEST_NAME + 'correct user shoud return 200']: (r) => r.status === 200,
         [TEST_NAME + 'correct user should return name']: (r) => r.json().data.name,
